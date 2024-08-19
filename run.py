d46047b348e6d046368a9dc1b619a98fc5bc0851
LingShan <f.tyt@foxmail.com>
2024-08-20 03:29:52 +0800

    new requirements.txt

--- run.py
+++ run.py
@@ -7,5 +7,6 @@ from clip_watcher import Watcher
 if __name__ == "__main__":
     url = "http://127.0.0.1:2024/"
     webbrowser.open(url, new=0, autoraise=True)
-    Watcher(model='qwen-vl-plus',Maas=dashscope).monitor()
     apple.run()
+    Watcher(model='qwen-vl-plus',Maas=dashscope).monitor()
+
